ac74b7f7774f21ff813830c42e7760ae78279781
hurli <khurliman26@gmail.com>
2025-01-16 14:14:56 -0500

    Third commit for today

--- src/com/neotech/gitNgithub/Practice1.java
+++ src/com/neotech/gitNgithub/Practice1.java
@@ -5,7 +5,8 @@ public class Practice1 {
 	public static void main(String[] args) {
 		
 		System.out.println("Hello World was the first class!");
-
+		
+		System.out.println("Another day, another practice");
 	}
 
 }
